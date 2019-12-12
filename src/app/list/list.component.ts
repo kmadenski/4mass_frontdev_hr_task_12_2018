271e6974251b61f14dbb258a7b6ab7c0ecb665e3
j <lorzechowski@protonmail.com>
2019-12-12 10:51:41 +0100

    list componet added material table

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Planet } from '../dummy';
+import { Component, OnInit, Input, ViewChild } from "@angular/core";
+import { Planet } from "../dummy";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { MainService } from "../main/main.service";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.scss"]
 })
 export class ListComponent implements OnInit {
-@Input() planetsList: Planet[] = [];
-  constructor() { }
+  @Input() planetsList: Planet[] = [];
+  @Input() searchForm: FormControl;
+  displayedColumns: string[] = [
+    "name",
+    "climate",
+    "terrain",
+    "diameter",
+    "gravity"
+  ];
+  dataSource;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  constructor(private _service: MainService) {}
 
   ngOnInit() {
+    this._service.fetchPlanets().subscribe(response => {
+      this.dataSource = new MatTableDataSource<Planet>(response.results);
+      this.dataSource.paginator = this.paginator;
+
+      this.searchForm.valueChanges.subscribe(val => this.applyFilter(val));
+    });
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
